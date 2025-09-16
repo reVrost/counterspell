980dcde4b34e0f011465f306db9a540f02776398
Kenley Bastari <kenley.bastari@immutable.com>
2025-09-16 16:27:38 +1000

    feat: header update

--- ui/src/components/MainHeader/MainHeader.tsx
+++ ui/src/components/MainHeader/MainHeader.tsx
@@ -1,16 +1,39 @@
 import { Group, Title } from "@mantine/core";
+import { useLocation } from "react-router-dom";
 
-export const MainHeader = () => (
-  <Group
-    justify="space-between"
-    align="center"
-    style={{
-      backgroundColor: "var(--mantine-color-gray-0)",
-      borderBottom: "1px solid var(--mantine-color-gray-2)",
-      boxShadow: "0.5px 0.5px  var(--mantine-color-gray-2)",
-    }}
-    p="12"
-  >
-    <Title order={3}>Header</Title>
-  </Group>
-);
+const getTitle = (pathname: string) => {
+  switch (pathname) {
+    case "/home":
+      return "Home";
+    case "/inferences":
+      return "Agents";
+    case "/logs":
+      return "Logs";
+    case "/metrics":
+      return "Metrics";
+    case "/settings":
+      return "Settings";
+    default:
+      return "Home";
+  }
+};
+
+export const MainHeader = () => {
+  const location = useLocation();
+  const title = getTitle(location.pathname);
+
+  return (
+    <Group
+      justify="space-between"
+      align="center"
+      style={{
+        backgroundColor: "var(--mantine-color-gray-0)",
+        borderBottom: "1px solid var(--mantine-color-gray-2)",
+        boxShadow: "0.5px 0.5px  var(--mantine-color-gray-2)",
+      }}
+      p="12"
+    >
+      <Title order={3}>{title}</Title>
+    </Group>
+  );
+};
