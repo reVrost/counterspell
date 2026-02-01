b7fbfb3c4aaa3d99f40dabb475fa47c9882c0e8e
reVrost <revrost@gmail.com>
2026-02-01 14:45:08 +1100

    feat: removed cs

--- counterspell/ui/src/routes/app/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	// Redirect /app to /dashboard
-	window.location.href = '/dashboard';
-</script>
-
-<noscript>
-	<meta http-equiv="refresh" content="0;url=/dashboard" />
-	<a href="/dashboard">Click here if you are not redirected.</a>
-</noscript>
